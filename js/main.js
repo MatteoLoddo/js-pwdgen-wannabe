05c4078b28115d20345e221724d5c3d8c9830417
MatteoLoddo <loddomatteo96@gmail.com>
2022-05-17 20:20:15 +0200

    fix per rispettare la richiesta

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 const nomeUtente = prompt (`Nome?`);
 const cognomeUtente = prompt (`Cognome?`);
-const annoNascita = prompt (`Anno di nascita?`);
+// const annoNascita = prompt (`Anno di nascita?`);
 const prefColor = prompt  (`Qual'è il tuo colore preferito?`);
-const annoAttuale = 2022;
-const eta = ( annoAttuale - annoNascita);
+// const annoAttuale = 2022;
+// const eta = ( annoAttuale - annoNascita);
+const  eta = (22)
+
+document.getElementById("info-user").innerHTML =`${nomeUtente}${cognomeUtente}${prefColor}${eta}`
 
 
-
-console.log(nomeUtente , cognomeUtente , eta , prefColor);
-
-
+/*
 document.getElementById("nome-utente").innerHTML =`Benvenuto ${nomeUtente} ${cognomeUtente}`;
 document.getElementById("actual-age").innerHTML =`Attualmente hai ${eta} anni!`;
 document.getElementById("colore-pref").innerHTML =`Ed il tuo colore preferito è il ${prefColor}`;
@@ -19,3 +19,6 @@ document.getElementById("colore-pref").innerHTML =`Ed il tuo colore preferito è
 document.getElementById("info-user").innerHTML =`<span> Benvenuto ${nomeUtente} ${cognomeUtente}</span><br>
                                                 <span>Attualmente hai ${eta} anni!</span><br>
                                                 <span>Ed il tuo colore preferito e il ${prefColor}</span>`
+*/
+console.dir(nomeUtente, cognomeUtente);
+
